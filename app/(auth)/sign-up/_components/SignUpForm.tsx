8ca56c330cfcce6575a4f4a24dd6b422594f66e2
Ifeanyi Ani <ifeanyiani155@gmail.com>
2024-08-09 22:05:53 +0100

    feat: setup our form in a multi step

--- app/(auth)/sign-up/_components/SignUpForm.tsx
+++ app/(auth)/sign-up/_components/SignUpForm.tsx
@@ -48,6 +48,136 @@ const SignUpForm = () => {
     },
   });
   const { isFirstStep, isLastStep, step, next, back, currentStepIndex } = useMutistepForm([
+    <FormField
+      key={1}
+      name='email'
+      control={form.control}
+      render={function ({ field }) {
+        return (
+          <FormItem key={field.name}>
+            <FormControl>
+              <Input placeholder='johndoe@gmail.com' {...field} />
+            </FormControl>
+            <FormMessage />
+            <FormDescription>
+              Already have an account?{' '}
+              <Link href='/login' className='text-purple-700'>
+                Login
+              </Link>
+            </FormDescription>
+          </FormItem>
+        );
+      }}
+    />,
+    <>
+      <FormField
+        key={2}
+        name='password'
+        control={form.control}
+        render={function ({ field }) {
+          return (
+            <FormItem key={field.name}>
+              <FormControl>
+                <Input
+                  type={showPassword ? 'text' : 'password'}
+                  placeholder='Enter password'
+                  {...field}
+                  onChange={(e) => {
+                    field.onChange(e);
+                    form.trigger('password');
+                  }}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          );
+        }}
+      />
+      <FormField
+        key={3}
+        name='confirmPassword'
+        control={form.control}
+        render={function ({ field }) {
+          return (
+            <div className='space-y-2'>
+              <FormItem>
+                <FormLabel>Confirm Password</FormLabel>
+                <FormControl>
+                  <Input
+                    type={showPassword ? 'text' : 'password'}
+                    placeholder='Re-type your password'
+                    {...field}
+                    onChange={(e) => {
+                      field.onChange(e);
+                      form.trigger('confirmPassword');
+                    }}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+              <div className='flex items-center space-x-2'>
+                <Checkbox id='password' onCheckedChange={() => setShowPassword((prev) => !prev)} />
+                <label
+                  htmlFor='password'
+                  className='cursor-pointer text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70'
+                >
+                  Show password
+                </label>
+              </div>
+            </div>
+          );
+        }}
+      />
+    </>,
+
+    <FormField
+      key={4}
+      name='businessName'
+      control={form.control}
+      render={function ({ field }) {
+        return (
+          <FormItem>
+            <FormControl>
+              <Input type='text' placeholder='Business name' {...field} />
+            </FormControl>
+            <FormMessage />
+          </FormItem>
+        );
+      }}
+    />,
+
+    <FormField
+      key={5}
+      name='country'
+      control={form.control}
+      render={function ({ field }) {
+        return (
+          <FormItem>
+            <Select onValueChange={field.onChange} defaultValue={field.value}>
+              <FormControl>
+                <SelectTrigger>
+                  <SelectValue placeholder='e.g Nigeria' />
+                </SelectTrigger>
+              </FormControl>
+              <SelectContent>
+                <SelectGroup>
+                  <SelectLabel>Countrys</SelectLabel>
+                  <SelectSeparator />
+                  {countryOptions.map((country, idx) => {
+                    return (
+                      <SelectItem key={idx} value={country.label}>
+                        {country.label}
+                      </SelectItem>
+                    );
+                  })}
+                </SelectGroup>
+              </SelectContent>
+            </Select>
+            <FormMessage />
+          </FormItem>
+        );
+      }}
+    />,
   ]);
 
   return (
@@ -56,6 +186,31 @@ const SignUpForm = () => {
         <CardHeading>
         </CardHeading>
         <CardContent>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className='space-y-9'>
+              {step}
+              <div className='absolute bottom-8 right-6 flex justify-end'>
+                {isLastStep ? (
+                  <Button
+                    label={'SignUp'}
+                    type='submit'
+                    isLoading={form.formState.isLoading}
+                    disabled={form.formState.isSubmitting}
+                    intent='secondary'
+                    className='bg-purple-300'
+                  />
+                ) : (
+                  <Button
+                    label='Next'
+                    type='button'
+                    onClick={handleNextStep}
+                    intent='secondary'
+                    className='bg-purple-300 text-white hover:bg-purple-500 focus-visible:bg-[none]'
+                  />
+                )}
+              </div>
+            </form>
+          </Form>
         </CardContent>
       </Card>
     </div>
