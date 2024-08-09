7e380805c318329c1ef33d5588ac8ae250278462
Ifeanyi Ani <ifeanyiani155@gmail.com>
2024-08-09 21:40:50 +0100

    styles: control the max-w

--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -2,3 +2,11 @@ import React from 'react';
 import LeftFormCard from '@/components/auth/LeftFormCard';
 import SignUpForm from '../_components/SignUpForm';
 
+const SignUpPage = () => {
+  return (
+    <div className='max-w-screen-[1440px] m-auto flex max-h-[1024px]  items-center bg-cs-gray-200'>
+    </div>
+  );
+};
+
+export default SignUpPage;
