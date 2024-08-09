96edc6123626b3d2f565e4582eb4e042be4d1c1f
Ifeanyi Ani <ifeanyiani155@gmail.com>
2024-08-09 21:42:12 +0100

    feat: display leftCard and SignUpForm component

--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -5,6 +5,12 @@ import SignUpForm from '../_components/SignUpForm';
 const SignUpPage = () => {
   return (
     <div className='max-w-screen-[1440px] m-auto flex max-h-[1024px]  items-center bg-cs-gray-200'>
+      <LeftFormCard
+        className='w-[720px]'
+        title='Productivity at its peak'
+        message="Where productivity meets collaboration at its peak. Elevate your work experience as Teamlyf seamlessly integrates tools and streamlines communication right at the core of your team's success."
+      />
+      <SignUpForm />
     </div>
   );
 };
