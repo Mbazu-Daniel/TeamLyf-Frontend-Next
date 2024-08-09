626dfdbac6aeac34f326c0112bd4def95a758024
Ifeanyi Ani <ifeanyiani155@gmail.com>
2024-08-09 22:23:56 +0100

    feat: auth Layout

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,12 @@
+export default function AuthLayout({
+  children, 
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <main className="relative grid min-h-dvh items-center justify-center py-8">
+      <div className="w-auth rounded-2xl bg-white">{children}</div>
+    </main>
+  );
+}
+
