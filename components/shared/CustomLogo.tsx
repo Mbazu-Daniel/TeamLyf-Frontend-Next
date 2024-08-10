e91d00232a44a699a4c6dbae7bac5c26cf5a8191
Ifeanyi Ani <ifeanyiani155@gmail.com>
2024-08-10 02:21:56 +0100

    update: move the cardHeading to customlogo component and add link to it

---
+++ components/shared/CustomLogo.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import Image from 'next/image';
+
+import { CardHeader } from '@/components/ui/card';
+import Link from 'next/link';
+type Props = {
+  children?: React.ReactNode;
+  className?: string;
+  width: number;
+  height: number;
+};
+
+export const CustomLogo = ({ children, className, width, height }: Props) => {
+  return (
+    <CardHeader className={className}>
+      <Link href='/'>
+        <Image src='/assets/lyfBlueLogo.svg' alt='Teamlyf Logo' width={width} height={height} />
+      </Link>
+      {children}
+    </CardHeader>
+  );
+};
