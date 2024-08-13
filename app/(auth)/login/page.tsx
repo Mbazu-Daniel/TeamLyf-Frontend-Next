6104af9bd432acc3f59c72d6d33f771e474ef356
Oladize1 <104131886+Oladize1@users.noreply.github.com>
2024-08-14 00:24:26 +0100

    Style: switched to provided input component

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -2,13 +2,15 @@
 
 import Link from "next/link";
 import { useState } from "react";
-import OpenEye from '../../../public/assets/eyeopenicon.svg';
-import CloseEye from '../../../public/assets/eyecloseicon.svg';
+import BaseAuthentication from "@/components/auth/BaseAuthentication"
+import OpenEye from "@/public/assets/eyeopenicon.svg";
+import CloseEye from "@/public/assets/eyecloseicon.svg";
 import LeftFormCard from "@/components/auth/LeftFormCard";
 import BaseFormCard from "@/components/auth/BaseFormCard";
 import BaseBtn from "@/components/auth/BaseBtn";
-import BaseAuthentication from "@/components/auth/BaseAuthentication";
 import { useForm, SubmitHandler } from "react-hook-form";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/Label";
 
 interface Inputs {
   email: string;
@@ -28,42 +30,49 @@ const LoginPage: React.FC = () => {
     <div className="min-h-screen flex flex-col justify-center md:grid md:grid-cols-2">
       <LeftFormCard
         title="Productivity at its peak"
-        message="Where productivity meets collaboration at its peak. Elevate your work experience as Teamlyf seamlessly integrates tools and streamlines communication right at the core of your team's success."
+        message="Teamlyf – where productivity meets collaboration at its peak. Elevate your work experience as Teamlyf seamlessly integrates tools and streamlines communication right at the core of your team's success."
       />
       <div className="form-pane pt-12 md:pt-0 bg-[#f0f5f8] px-8 md:px-24 flex flex-col justify-end pb-12">
         <BaseFormCard heading="Login" title="Securely login to">
           <form onSubmit={handleSubmit(onSubmit)} noValidate className="input-section flex gap-4 flex-col">
-            <div>
-              <label htmlFor="email" className="block text-sm font-medium text-gray-700">Email Address</label>
-              <input
-                type="email"
-                placeholder="Example@gmail.com"
-                {...register("email", { required: { value: true, message: "Please input a valid email" } })}
-                className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
-              />
-              <p className="text-red-500 text-sm mt-1">{errors.email?.message}</p>
-            </div>
-            <div className="relative">
-              <label htmlFor="password" className="block text-sm font-medium text-gray-700">Password</label>
-              <input
-                type={showPassword ? "text" : "password"}
-                placeholder="Your password"
-                {...register("password", { required: { value: true, message: "Please provide a password" } })}
-                className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
-              />
-              <button
-                type="button"
-                onClick={() => setShowPassword((prevShow) => !prevShow)}
-                className="absolute inset-y-0 right-0 pr-3 flex items-center text-sm leading-5"
-              >
-                {showPassword ? <img src={OpenEye} className="w-5 h-5" alt="Hide password" /> : <img src={CloseEye} className="w-5 h-5" alt="Show password" />}
-              </button>
-              <p className="text-red-500 text-sm mt-1">{errors.password?.message}</p>
-            </div>
+            
+            {/* Email Input Field */}
+            <Label htmlFor="email">Email Address</Label>
+            <Input
+              type="email"
+              placeholder="Example@gmail.com"
+              {...register("email", { required: "Please input a valid email" })}
+              intent="default"
+              inputSize="md"
+              error={errors.email?.message}
+            />
+            
+            {/* Password Input Field */}
+            <Label htmlFor="password">Password</Label>
+            <Input
+              type={showPassword ? "text" : "password"}
+              placeholder="Your password"
+              {...register("password", { required: "Please provide a password" })}
+              intent="default"
+              inputSize="md"
+              rightIcon={
+                <button
+                  type="button"
+                  onClick={() => setShowPassword((prevShow) => !prevShow)}
+                  className="text-sm leading-5"
+                >
+                  {showPassword ? <img src={OpenEye} className="w-5 h-5" alt="Hide password" /> : <img src={CloseEye} className="w-5 h-5" alt="Show password" />}
+                </button>
+              }
+              error={errors.password?.message}
+            />
+
             <Link href="#" className="text-right text-gray-600 text-sm">
               Forgot Password
             </Link>
+
             <BaseBtn btnTitle="Login" />
+
           </form>
           <BaseAuthentication />
           <div className="text-center mt-4">
