bc978406d161e4f43889c507f29dc7d80874a6a8
Ifeanyi Ani <ifeanyiani155@gmail.com>
2024-09-05 12:11:25 +0100

    account component

---
+++ components/Account.tsx
@@ -0,0 +1,60 @@
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { ChevronDown } from "lucide-react";
+
+interface Props {
+  title: string;
+  description: string;
+  src: string;
+}
+export const Account = ({
+  title,
+  description,
+  src = "/assets/icons/avatar.svg",
+}: Props) => {
+  const getAbbrevation = (str: string) => {
+    return str
+      .split(" ")
+      .map((name) => name[0])
+      .join("")
+      .toUpperCase();
+  };
+
+  const fallBack = getAbbrevation(title);
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger className="flex gap-2 items-center">
+        <Avatar>
+          <AvatarImage src={src} />
+
+          <AvatarFallback>{fallBack}</AvatarFallback>
+        </Avatar>
+
+        <div className="flex flex-col justify-center items-start">
+          <h5 className="text-primary text-sm font-bold">{title}</h5>
+          <p className="text-muted-foreground text-xs">{description}</p>
+        </div>
+
+        <ChevronDown className="w-6 h-6 text-muted-foreground" />
+      </DropdownMenuTrigger>
+      <DropdownMenuContent>
+        <DropdownMenuLabel>My Account</DropdownMenuLabel>
+
+        <DropdownMenuSeparator />
+
+        <DropdownMenuItem>Profile</DropdownMenuItem>
+        <DropdownMenuItem>Workspace</DropdownMenuItem>
+        <DropdownMenuItem>Team</DropdownMenuItem>
+        <DropdownMenuItem>Settings</DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
