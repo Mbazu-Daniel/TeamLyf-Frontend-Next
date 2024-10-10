3c5e95a341b448cb3263b6a56f1cb60b87e75ade
Godwin <boiikechukwu.godwin@gmail.com>
2024-10-10 17:23:56 -0400

    completed the chat sections

--- app/(workspace)/[slug]/chats/page.tsx
+++ app/(workspace)/[slug]/chats/page.tsx
@@ -52,10 +52,14 @@ async function fetchGroups (e: string): Promise<Query[]> {
 export default function Chat () {
   const [querys, setQuerys] = useState<Query[]>([])
   const [tabTerm, setTabTerm] = useState<string>('Direct')
+  const [pinned] = useState<boolean>(false)
 
   const handleTabChange = (value: string): void => {
     setTabTerm(value)
   }
+  // const handlePinChange = (e: any): void => {
+  //   setPinned(e.toggle(true))
+  // }
 
   useEffect(() => {
     const fetchData = async (): Promise<void> => {
@@ -165,13 +169,23 @@ export default function Chat () {
                                 <div className='w-8/12 h-20  flex gap-2 items-center'>
                                   {/* Avatar and online status */}
                                   <div className='relative'>
-                                    <Avatar className='w-12 h-12'>
-                                      <AvatarImage
-                                        src='https://github.com/shadcn.png'
-                                        alt='@shadcn'
-                                      />
-                                      <AvatarFallback>CN</AvatarFallback>
-                                    </Avatar>
+                                    {tabTerm === 'Groups' ? (
+                                      <Avatar className='w-12 h-12 rounded-md shrink-0'>
+                                        <AvatarImage
+                                          src='https://github.com/shadcn.png'
+                                          alt='@shadcn'
+                                        />
+                                        <AvatarFallback>CN</AvatarFallback>
+                                      </Avatar>
+                                    ) : (
+                                      <Avatar className='w-12 h-12 shrink-0'>
+                                        <AvatarImage
+                                          src='https://github.com/shadcn.png'
+                                          alt='@shadcn'
+                                        />
+                                        <AvatarFallback>CN</AvatarFallback>
+                                      </Avatar>
+                                    )}
 
                                     {/* online status */}
                                     {tabTerm === 'Direct' &&
@@ -240,52 +254,70 @@ export default function Chat () {
             <div className='flex w-full h-20 px-4'>
               {/* Private chats activated */}
               <div className='w-4/12 flex gap-3 items-center'>
-                <Avatar>
-                  <AvatarImage
-                    src='https://github.com/shadcn.png'
-                    alt='@shadcn'
-                  />
-                  <AvatarFallback>CN</AvatarFallback>
-                </Avatar>
-                <div className=''>
-                  <h2 className='text-base font-bold text-black-900'>
-                    Adura O.
-                  </h2>
-                  <p className='text-xs text-black-900 opacity-50 -mt-0.5'>
-                    @aduraOgunlade
-                  </p>
-                </div>
+                {tabTerm === 'Groups' ? (
+                  <>
+                    <Avatar className='w-12 h-12 rounded-md shrink-0'>
+                      <AvatarImage
+                        src='https://github.com/shadcn.png'
+                        alt='@shadcn'
+                      />
+                      <AvatarFallback>CN</AvatarFallback>
+                    </Avatar>
+                    <div className=''>
+                      <h2 className='text-base font-bold text-black-900'>
+                        Adura O.
+                      </h2>
+                      {/* this region to change letter in the future, should be according to the number of those who is online at the moment  */}
+                      <div className='text-xs text-black-900 opacity-50 -mt-0.5 flex gap-3'>
+                        <p className='font-semibold'>
+                          <span>8</span> members
+                        </p>
+                        <div className='flex gap-2 items-center'>
+                          <p className='w-1.5 h-1.5 rounded-full bg-gray-900'></p>
+                          <p className='font-semibold text-green-600'>
+                            <span>3</span> online
+                          </p>
+                        </div>
+                      </div>
+                    </div>
+                  </>
+                ) : (
+                  <>
+                    <Avatar className='w-12 h-12 shrink-0'>
+                      <AvatarImage
+                        src='https://github.com/shadcn.png'
+                        alt='@shadcn'
+                      />
+                      <AvatarFallback>CN</AvatarFallback>
+                    </Avatar>
+                    <div className=''>
+                      <h2 className='text-base font-bold text-black-900'>
+                        Adura O.
+                      </h2>
+                      <p className='text-xs text-black-900 opacity-50 -mt-0.5'>
+                        @aduraOgunlade
+                      </p>
+                    </div>
+                  </>
+                )}
               </div>
-
-              {/* Group settings activated */}
-              {/* <div className='w-4/12 flex gap-3 items-center'>
-                <Avatar className='w-12 h-12 rounded-md'>
-                  <AvatarImage
-                    src='https://github.com/shadcn.png'
-                    alt='@shadcn'
-                  />
-                  <AvatarFallback>CN</AvatarFallback>
-                </Avatar>
-                <div className=''>
-                  <h2 className='text-base font-bold text-black-900'>
-                    Adura O.
-                  </h2>
-                  <p className='text-xs text-black-900 opacity-50'>
-                    @aduraOgunlade
-                  </p>
-                </div>
-              </div> */}
 
               <div className='w-8/12 flex items-center gap-3 justify-end'>
                 <div className='flex gap-2 content-center text-center justify-center text-base text-purple-800 align-middle'>
-                  <PlusIcon className='p-1 bg-gray-600 rounded w-5 h-5' />
-                  Add to a team
+                  {tabTerm === 'Groups' ? (
+                    <></>
+                  ) : (
+                    <>
+                      <PlusIcon className='p-1 bg-gray-600 rounded w-5 h-5' />
+                      Add to a team
+                    </>
+                  )}
                 </div>
                 <div className='relative '>
                   <SearchInput
-                    leftIcon={<SearchIcon className='w-5 h-5 opacity-50' />}
+                    leftIcon={<SearchIcon className='w-6 h-6 opacity-50' />}
                     placeHolder='Search...'
-                    // className='w-80'
+                    className='w-60'
                   />
                 </div>
                 <div className='flex gap-1 content-center'>
@@ -298,71 +330,17 @@ export default function Chat () {
             </div>
             {/* chat area where conversations are displayed */}
             <section className=' pt-0.5 h-[817px] w-full'>
-              {/* pinned chat area */}
-              {/* <div className='flex gap-4 px-8 items-center h-14 bg-[#fdf7fc] '>
-                <div>
-                  <PipetteIcon className='w-6 h-6 text-gray-900' />
-                </div>
-                <div className='relative'>
-                  <Avatar className='w-8 h-8'>
-                    <AvatarImage
-                      src='https://github.com/shadcn.png'
-                      alt='@shadcn'
-                    />
-                    <AvatarFallback>CN</AvatarFallback>
-                  </Avatar>
-                </div>
-                <div>
-                  <div className=' flex gap-8 items-center'>
-                    <h2 className='text-sm font-normal text-blue-500'>
-                      Marcus Felix
-                    </h2>
-                    <ul className='text-sm text-black text-opacity-80 font-medium'>
-                      <li>
-                        Hey everybody, do not forget the review meeting coming
-                        up 8px this saturday!
-                      </li>
-                    </ul>
-                  </div>
-                </div>
-              </div> */}
-
               <ScrollArea className='h-[789px] mb-4 mx-6'>
                 <div className='w-full h-full relative'>
                   <div className='absolute rounded-lg w-fit px-3 py-1.5 top-6 left-1/2 transform translate-x-1/2 bg-white text-black text-opacity-70 text-sm'>
                     Today
                   </div>
                   {/* pinned chat area */}
-                  {/* <div className='flex gap-2 items-center h-14 bg-[#fdf7fc] '>
-                    <div>
-                      <PipetteIcon className='w-6 h-6 text-gray-900' />
-                    </div>
-                    <div className='relative'>
-                      <Avatar className='w-8 h-8'>
-                        <AvatarImage
-                          src='https://github.com/shadcn.png'
-                          alt='@shadcn'
-                        />
-                        <AvatarFallback>CN</AvatarFallback>
-                      </Avatar>
-                    </div>
-                    <div>
-                      <div className=' flex gap-8 items-center'>
-                        <h2 className='text-sm font-normal text-blue-500'>
-                          Marcus Felix
-                        </h2>
-                        <ul className='text-sm text-black text-opacity-80 font-medium'>
-                          <li>
-                            Hey everybody, do not forget the review meeting
-                            coming up 8px this saturday!
-                          </li>
-                        </ul>
+                  {pinned && tabTerm === 'Groups' ? (
+                    <div className='flex gap-2 items-center h-14 bg-[#fdf7fc] '>
+                      <div>
+                        <PipetteIcon className='w-6 h-6 text-gray-900' />
                       </div>
-                    </div>
-                  </div> */}
-                  {/* chats body area */}
-                  <div className='w-ful justify-start pt-4'>
-                    <div className='flex gap-2 items-center'>
                       <div className='relative'>
                         <Avatar className='w-8 h-8'>
                           <AvatarImage
@@ -374,41 +352,96 @@ export default function Chat () {
                       </div>
                       <div>
                         <div className=' flex gap-8 items-center'>
-                          <h2 className='text-sm font-normal text-red-500'>
-                            Adura Ogunlara
+                          <h2 className='text-sm font-normal text-blue-500'>
+                            Marcus Felix
                           </h2>
-                          <ul className='list-disc text-sm text-gray-700'>
+                          <ul className='text-sm text-black text-opacity-80 font-medium'>
                             <li>
-                              03:45<span>PM</span>
+                              Hey everybody, do not forget the review meeting
+                              coming up 8px this saturday!
                             </li>
                           </ul>
                         </div>
                       </div>
                     </div>
-                    <div className='ml-10'>
-                      <ul className='text-sm mb-1.5'>
-                        <li>
-                          That's nice, just wanted to ask you about your
-                          progress on the admin page for teamify project
-                        </li>
-                        <li>
-                          That's nice, just wanted to ask you about your
-                          progress on the admin page for teamify project
-                        </li>
-                      </ul>
+                  ) : (
+                    <></>
+                  )}
+
+                  {/* chats body area */}
+                  {/* First we have to check the length of the files in the object since we fetching dynamically
+                  if it is not empty, it should then check to see if there are any comments in each indexed file */}
+                  {querys.length > 0 ? (
+                    querys.map((query, i) =>
+                      query.comments.length > 0 ? (
+                        query.comments.map(comment => (
+                          <div
+                            key={comment.postid}
+                            className='w-full justify-start pt-4'
+                          >
+                            <div className='flex gap-2 items-center'>
+                              <div className='relative'>
+                                <Avatar className='w-8 h-8'>
+                                  <AvatarImage
+                                    src='https://github.com/shadcn.png'
+                                    alt='@shadcn'
+                                  />
+                                  <AvatarFallback>CN</AvatarFallback>
+                                </Avatar>
+                              </div>
+                              <div>
+                                <div className=' flex gap-8 items-center'>
+                                  <h2 className='text-sm font-normal text-red-500'>
+                                    {query.name}
+                                  </h2>
+                                  <ul className='list-disc text-sm text-gray-700'>
+                                    <li>{query.time}</li>
+                                  </ul>
+                                </div>
+                              </div>
+                            </div>
+                            <div className='ml-10'>
+                              <ul className='text-sm mb-1.5'>
+                                <li>{comment.body}</li>
+                              </ul>
+                            </div>
+                          </div>
+                        ))
+                      ) : // Checking to see if the comments are empty and the query exceeds one
+                      i > 0 ? (
+                        // note: the code has some bug but this is just a mock, api call annd structure will change later so i will leave it like this for now
+                        // this prevents the first time chatting box not to display multiple times
+                        <></>
+                      ) : (
+                        // this shows if there are not comments when the first comment obj of the query obj is empty
+                        <div className='h-[789px] grid place-content-center'>
+                          <div
+                            key={i}
+                            className='flex flex-col items-center justify-center text-center w-full h-full'
+                          >
+                            <MessyIcon className='mb-6' />
+                            <p className='text-gray-500 text-base'>
+                              This is your first time chatting
+                            </p>
+                          </div>
+                        </div>
+                      )
+                    )
+                  ) : (
+                    // this shows if no query exists in the first place or rather first time chatting
+                    <div className='h-[789px] grid place-content-center'>
+                      <div className='flex flex-col items-center justify-center text-center w-full h-full'>
+                        <MessyIcon className='mb-6' />
+                        <p className='text-gray-500 text-base'>
+                          This is your first time chatting
+                        </p>
+                      </div>
                     </div>
-                  </div>
+                  )}
                 </div>
-                {/* if no chats or rather first time chatting, this will show on the chat area saying 'first time chatting' */}
-                {/* <div className='flex flex-col items-center justify-center text-center w-full h-full'>
-                  <MessyIcon className='mb-6' />
-                  <p className='text-gray-500 text-base'>
-                    This is your first time chatting
-                  </p>
-                </div> */}
               </ScrollArea>
               {/* chat area located at the bottom of the chat display container where messages are typed and forwarded */}
-              <div className='flex items-center justify-between bg-white rounded-xl px-4 py-2'>
+              <div className='flex items-center justify-between bg-white rounded-xl px-4 py-2 mx-6'>
                 <Input
                   type='text'
                   placeHolder='Leave a message for Adura'
